d09f794a45ae8b2973ac6d98184ce8d96b0a59ca
joykepler <joyjoykepler@gmail.com>
2025-01-07 21:38:21 -0300

    create task crud

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 Route::get('/status', function () {
     return response()->json(['status' => 'Running']);
 });
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::put('/tasks/{task}', [TaskController::class, 'update']);
